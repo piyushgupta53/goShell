e6295c238f157e421396f47579cf3532a93ed243
Piyush Gupta <PIYUSHGUPTA53@GMAIL.COM>
2025-03-30 16:31:18 +0530

    feat: added tokenize function for input

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -5,7 +5,7 @@ import "strings"
 // Parse splits the input string into a Command (command + args)
 // e.g. input: "ls -ls/tmp" => Command{Name: "ls", Args: ["-la", "/tmp"]}
 func Parse(input string) *Command {
-	tokens := strings.Fields(input)
+	tokens := tokenize(input)
 
 	if len(tokens) == 0 {
 		return nil
@@ -15,4 +15,60 @@ func Parse(input string) *Command {
 		Name: tokens[0],
 		Args: tokens[1:],
 	}
+}
+
+func tokenize(input string) []string {
+	var tokens []string
+	var current strings.Builder
+	inSingleQuote := false
+	inDoubleQuote := false
+	escaped := false
+
+	for i := 0; i < len(input); i++ {
+		ch := input[i]
+
+		if escaped {
+			current.WriteByte(ch)
+			escaped = false
+			continue
+		}
+
+		switch ch {
+		case '\\':
+			escaped = true
+
+		case '\'':
+			if inDoubleQuote {
+				current.WriteByte(ch)
+			} else {
+				inSingleQuote = !inSingleQuote
+			}
+
+		case '"':
+			if inSingleQuote {
+				current.WriteByte(ch)
+			} else {
+				inDoubleQuote = !inDoubleQuote
+			}
+
+		case ' ', '\t':
+			if inSingleQuote || inDoubleQuote {
+				current.WriteByte(ch)
+			} else if current.Len() > 0 {
+				tokens = append(tokens, current.String())
+				current.Reset()
+			}
+
+		default:
+			current.WriteByte(ch)
+		}
+
+	}
+
+	// flush last tokens
+	if current.Len() > 0 {
+		tokens = append(tokens, current.String())
+	}
+
+	return tokens
 }
